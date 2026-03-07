bd03ffbf04629e9bf07a2c0576c6210fd725cbe0
𝗦𝗜𝗚𝗠𝗔.𝗘𝗫𝗢𝗧𝗨𝗦.𝗧𝗘𝗖𝗛.𝗗𝗘𝗩 <jeandoberto55@gmail.com>
2026-03-07 08:53:44 -0800

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: 'QUEEN VALENTINA MD',
+  author: 'LRD',
+  botName: "QUEEN VALENTINA MD",
+  botOwner: 'DEVSIGMA', // Your name
+  ownerNumber: '24105075484', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "3.0.9",
+  updateZipUrl: "https://github.com/TECHGOAT333/LASER-MD/archive/refs/heads/main.zip'",
+};
+
+module.exports = settings;
