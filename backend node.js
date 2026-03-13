422884b1455f0e90d5903b3fdd1e281ac5ad3ab5
𝗦𝗜𝗚𝗠𝗔.𝗘𝗫𝗢𝗧𝗨𝗦.𝗧𝗘𝗖𝗛.𝗗𝗘𝗩 <jeandoberto55@gmail.com>
2026-03-13 13:09:40 -0500

    Create backend node.js

---
+++ backend node.js
@@ -0,0 +1,11 @@
+const { default: makeWASocket } = require("@whiskeysockets/baileys")
+
+async function startBot() {
+
+const sock = makeWASocket({
+    printQRInTerminal: false
+})
+
+}
+
+startBot()
