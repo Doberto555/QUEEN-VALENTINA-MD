44c98db779b7ab5d323d04b6e07ce13fdfaac857
𝗦𝗜𝗚𝗠𝗔.𝗘𝗫𝗢𝗧𝗨𝗦.𝗧𝗘𝗖𝗛.𝗗𝗘𝗩 <jeandoberto55@gmail.com>
2026-03-20 03:46:43 -0500

    Update sticker.js

--- commands/sticker.js
+++ commands/sticker.js
@@ -36,8 +36,8 @@ async function stickerCommand(sock, chatId, message) {
                 forwardingScore: 999,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363407561123100@newsletter',
-                    newsletterName: 'LASER-MD',
+                    newsletterJid: '120363423792937578@newsletter',
+                    newsletterName: 'QUEEN VALENTINA-MD',
                     serverMessageId: -1
                 }
             }
@@ -58,8 +58,8 @@ async function stickerCommand(sock, chatId, message) {
                     forwardingScore: 999,
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
-                        newsletterJid: '120363407561123100@newsletter',
-                        newsletterName: 'LASER-MD',
+                        newsletterJid: '120363423792937578@newsletter',
+                        newsletterName: 'QUEEN VALENTINA-MD',
                         serverMessageId: -1
                     }
                 }
@@ -216,8 +216,8 @@ async function stickerCommand(sock, chatId, message) {
                 forwardingScore: 999,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363407561123100@newsletter',
-                    newsletterName: 'LASER-MD',
+                    newsletterJid: '120363423792937578@newsletter',
+                    newsletterName: 'QUEEN VALENTINA-MD',
                     serverMessageId: -1
                 }
             }
